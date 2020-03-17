87f0bbee4a519829fad2e15cc2bf2da94a50871c
Brian Kenny <brian@minicorp.ie>
2020-03-17 17:02:38 +0000

    Final routing

--- app/controllers/practice_forms_controller.rb
+++ app/controllers/practice_forms_controller.rb
@@ -5,5 +5,47 @@ class PracticeFormsController < ApplicationController
   end
 
   def create
+    @practice = Practice.friendly.find(params[:practice])
+    @practice_form = @practice.practice_forms.create(practice_form_params)
+
+    if @practice_form.persisted?
+      redirect_to practice_form_complete_path(practice: @practice.slug), flash: {
+          notice: "Form submitted successfully.",
+        }
+    else
+      flash[:alert] = @practice_form.errors.full_messages.join(". ")
+      render action: :new
+    end
+  end
+
+  def complete
+    @practice = Practice.friendly.find(params[:practice])
+  end
+
+  private
+
+  def practice_form_params
+    params.require(:practice_form)
+      .permit(
+        :practice_id,
+        :first_name,
+        :last_name,
+        :dob,
+        :address_1,
+        :address_2,
+        :city,
+        :county,
+        :postcode,
+        :country,
+        :email,
+        :message,
+        :symptoms,
+        :symptoms_duration,
+        :severity,
+        :recent_travel,
+        :self_isolating,
+        :symptoms_outline,
+        :pre_existing_medical_conditions
+      )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/:practice', to: 'practice_forms#new', as: 'new_practice_form'
   post '/:practice/submit', to: 'practice_forms#create', as: 'submit_practice_form'
+  get '/:practice/complete', to: 'practice_forms#complete', as: 'practice_form_complete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"
