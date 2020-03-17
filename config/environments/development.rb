40c85264665f0a7687321000dff97be024fb5b6a
Brian Kenny <brian@minicorp.ie>
2020-03-17 15:17:53 +0000

    Building up practices model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,11 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.default_url_options = { host: "localhost", port: 5000 }
+
+  # Open emails in the browser instantly
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.asset_host = 'localhost:5000'
 end
