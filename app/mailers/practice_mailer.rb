e29c5a3fac7fe07c22be5f8bdeb925bb11349164
Brian Kenny <brian@minicorp.ie>
2020-03-19 10:45:55 +0000

    Set the to and from correctly

--- app/mailers/practice_mailer.rb
+++ app/mailers/practice_mailer.rb
@@ -4,8 +4,8 @@ class PracticeMailer < ApplicationMailer
     mail(to: @practice_form.practice.email, subject: 'Screening Form Submission')
 
     make_bootstrap_mail(
-      to: 'Screening Form <no-reply@screeningform.com>',
-      from: @practice_form.practice.email,
+      to: @practice_form.practice.email,
+      from: 'Screening Form <no-reply@screeningform.com>',
       bcc: 'brian@minicorphq.com',
       subject: 'Screening Form Submission'
     )
