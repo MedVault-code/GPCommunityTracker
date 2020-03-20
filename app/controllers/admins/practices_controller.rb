5db74c975b08409d854ee5be3494619081b1ac8e
Brian Kenny <brian@minicorp.ie>
2020-03-20 13:43:09 +0000

    Add admin section

--- app/controllers/admins/practices_controller.rb
+++ app/controllers/admins/practices_controller.rb
@@ -3,5 +3,47 @@ module Admins
     def index
       @practices = Practice.all.order('created_at desc')
     end
+
+    def edit
+      @practice = Practice.friendly.find(params[:id])
+    end
+
+    def update
+      @practice = Practice.friendly.find(params[:id])
+
+      if @practice.update(practice_params)
+        redirect_to admins_practices_path, notice: "Practice successfully updated"
+      else
+        flash[:alert] = @practice.errors.full_messages.to_sentence
+        render action: :edit
+      end
+    end
+
+    def destroy
+      @practice = Practice.friendly.find(params[:id])
+
+      if @practice.destroy
+        @message = 'Practice successfully deleted'
+      else
+        @message = @practice.errors.full_messages.to_sentence
+      end
+
+      redirect_to admins_practices_path(@building), notice: @message
+    end
+
+    private
+
+    def practice_params
+      params.require(:practice)
+        .permit(
+          :first_name,
+          :last_name,
+          :practice_name,
+          :email,
+          :practice_phone,
+          :address,
+          :slug,
+        )
+    end
   end
 end
--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,9 +1,4 @@
 module Admins
   class SessionsController < Devise::SessionsController
-    protected
-
-    def after_sign_in_path_for(resource)
-      stored_location_for(resource) || :admins_root
-    end
   end
 end
