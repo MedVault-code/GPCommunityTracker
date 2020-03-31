a004132198120edbf0e76fc4495e2cc404e24c2a
Kirk <ken@nustechnology.com>
2020-03-31 14:04:03 +0700

    Add testimonal content to home page

--- spec/integrations/home_page_spec.rb
+++ spec/integrations/home_page_spec.rb
@@ -4,5 +4,6 @@ describe "static page home", type: :feature do
   it "has basic content" do
     visit(root_path)
     expect(page).to have_content "COVID-19 GP Screening Form"
+    expect(page).to have_content "We are a small practice in Cork city with no practice website."
   end
 end
