2890b7fa8d19164ae83b14244c6676e6ab816c72
Kirk <ken@nustechnology.com>
2020-03-31 11:49:09 +0700

    Update test for home page

--- spec/integrations/home_page_spec.rb
+++ spec/integrations/home_page_spec.rb
@@ -4,5 +4,6 @@ describe "static page home", type: :feature do
   it "has basic content" do
     visit(root_path)
     expect(page).to have_content "COVID-19 GP Screening Form"
+    expect(page).to have_content "Join the over 300 practices across Ireland already using screening form today"
   end
 end
