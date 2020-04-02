5359a739fbb8494cb8438426a3e1d311a270b569
Alan Haverty <alan@minicorphq.com>
2020-04-02 17:19:34 +0100

    Fix constants with two words

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -10,29 +10,29 @@ IRELAND_COUNTIES = %w[
   Derry
   Donegal
   Down
-  Co. Dublin
-  Dublin 1
-  Dublin 2
-  Dublin 3
-  Dublin 4
-  Dublin 5
-  Dublin 6
-  Dublin 6W
-  Dublin 7
-  Dublin 8
-  Dublin 9
-  Dublin 10
-  Dublin 11
-  Dublin 12
-  Dublin 13
-  Dublin 14
-  Dublin 15
-  Dublin 16
-  Dublin 17
-  Dublin 18
-  Dublin 20
-  Dublin 22
-  Dublin 24
+  'Co. Dublin'
+  'Dublin 1'
+  'Dublin 2'
+  'Dublin 3'
+  'Dublin 4'
+  'Dublin 5'
+  'Dublin 6'
+  'Dublin 6W'
+  'Dublin 7'
+  'Dublin 8'
+  'Dublin 9'
+  'Dublin 10'
+  'Dublin 11'
+  'Dublin 12'
+  'Dublin 13'
+  'Dublin 14'
+  'Dublin 15'
+  'Dublin 16'
+  'Dublin 17'
+  'Dublin 18'
+  'Dublin 20'
+  'Dublin 22'
+  'Dublin 24'
   Fermanagh
   Galway
   Kerry
