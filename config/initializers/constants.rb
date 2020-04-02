7328d1a81f8bf8e4427542f419fd6fe08fb8fe89
Alan Haverty <alan@minicorphq.com>
2020-04-02 17:13:40 +0100

    Replace dublin with postcodes

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 IRELAND_COUNTIES = %w[
   Antrim
   Armagh
@@ -8,7 +10,29 @@ IRELAND_COUNTIES = %w[
   Derry
   Donegal
   Down
-  Dublin
+  Co. Dublin
+  Dublin 1
+  Dublin 2
+  Dublin 3
+  Dublin 4
+  Dublin 5
+  Dublin 6
+  Dublin 6W
+  Dublin 7
+  Dublin 8
+  Dublin 9
+  Dublin 10
+  Dublin 11
+  Dublin 12
+  Dublin 13
+  Dublin 14
+  Dublin 15
+  Dublin 16
+  Dublin 17
+  Dublin 18
+  Dublin 20
+  Dublin 22
+  Dublin 24
   Fermanagh
   Galway
   Kerry
@@ -32,4 +56,4 @@ IRELAND_COUNTIES = %w[
   Wexford
   Wicklow
   Other
-]
+].freeze
